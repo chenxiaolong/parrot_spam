262941b114434934947fb10892dbbaec2d92af4e
Andrew Gunnerson <annerson@us.ibm.com>
2017-01-06 14:38:33 -0500

    parrot_spam.cpp: Fix infinite loop if every argument is empty

--- parrot_spam.cpp
+++ parrot_spam.cpp
@@ -8,15 +8,17 @@ static const size_t MAX_CHARS = 4000;
 
 int main(int argc, char *argv[])
 {
-    if (argc < 2) {
-        std::cerr << "Nothing to repeat" << std::endl;
-        return EXIT_FAILURE;
-    }
-
     std::vector<std::string> items;
+    size_t total = 0;
 
     for (int i = 1; i < argc; ++i) {
         items.push_back(argv[i]);
+        total += items.back().size();
+    }
+
+    if (total == 0) {
+        std::cerr << "Nothing to repeat" << std::endl;
+        return EXIT_FAILURE;
     }
 
     size_t avail = MAX_CHARS;
