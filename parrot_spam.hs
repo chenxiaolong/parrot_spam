4ca4e00ac149897da0d541e73dd1168c5f09f0b1
Shlomo Shuck <sjshuck@us.ibm.com>
2017-01-11 14:12:51 -0500

    Haskell: Favor infinite lists over explicit tail recursion

--- parrot_spam.hs
+++ parrot_spam.hs
@@ -1,4 +1,5 @@
 import Control.Monad
+import Data.List
 import System.Environment
 import System.Exit
 import System.Posix.Terminal
@@ -9,10 +10,10 @@ main = do
     when (all null args) $ die "Nothing to repeat"
 
     isatty <- queryTerminal 1
-    (if isatty then putStrLn else putStr) $ parrots args 4000
+    (if isatty then putStrLn else putStr) $ limitByTotalLength 4000 $ cycle args
 
-parrots :: [String] -> Int -> String
-parrots = go . cycle where
-    go (s : ss) n
-        | n < length s = ""
-        | otherwise    = s ++ go ss (n - length s)
+withTotalLengths :: [String] -> [(String, Int)]
+withTotalLengths = zip <*> tail . scanl' (+) 0 . map length
+
+limitByTotalLength :: Int -> [String] -> [String]
+limitByTotalLength lim = map fst . takeWhile ((<= lim) . snd) . withTotalLengths
