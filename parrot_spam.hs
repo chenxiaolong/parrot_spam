e7695d2b6e5511758c3e867c5400448239505a9e
Shlomo Shuck <sjshuck@us.ibm.com>
2017-01-11 14:27:19 -0500

    Haskell: Forgot to concat

--- parrot_spam.hs
+++ parrot_spam.hs
@@ -10,7 +10,8 @@ main = do
     when (all null args) $ die "Nothing to repeat"
 
     isatty <- queryTerminal 1
-    (if isatty then putStrLn else putStr) $ limitByTotalLength 4000 $ cycle args
+    let display = if isatty then putStrLn else putStr
+    display $ concat $ limitByTotalLength 4000 $ cycle args
 
 withTotalLengths :: [String] -> [(String, Int)]
 withTotalLengths = zip <*> tail . scanl' (+) 0 . map length
