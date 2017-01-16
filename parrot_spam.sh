417f38114a76b60af37a1ae8dd56c34eacdac989
Shlomo Shuck <sjshuck@us.ibm.com>
2017-01-16 18:52:48 -0500

    Bash: Use cute Bash features

--- parrot_spam.sh
+++ parrot_spam.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
 avail=4000
-total=0
 
-for item in "${@}"; do
-    let "total += ${#item}"
-done
-
-if [[ "${total}" -eq 0 ]]; then
+ifs="$IFS"; IFS=""
+if [ -z "$*" ]; then
     echo 'Nothing to repeat' >&2
     exit 1
 fi
+IFS="$ifs"; unset ifs
 
 while true; do
     for item in "${@}"; do
-        if [[ "${avail}" -lt "${#item}" ]]; then
+        (( avail -= ${#item} ))
+        if (( avail < 0 )); then
             exit 0
         fi
+
         echo -n "${item}"
-        let "avail -= ${#item}"
     done
 done
