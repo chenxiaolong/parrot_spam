edd365b28a3bbbca13e7976a53ce622a500530fe
Shlomo Shuck <sjshuck@us.ibm.com>
2017-01-17 00:55:51 -0500

    Bash: Newline if interactive
    Bash: Even more cute

--- parrot_spam.sh
+++ parrot_spam.sh
@@ -2,20 +2,27 @@
 
 avail=4000
 
-ifs="$IFS"; IFS=""
+ifs="$IFS"
+IFS=""
 if [ -z "$*" ]; then
     echo 'Nothing to repeat' >&2
     exit 1
 fi
-IFS="$ifs"; unset ifs
+IFS="$ifs"
+unset ifs
 
 while true; do
-    for item in "${@}"; do
+    for item in "$@"; do
         (( avail -= ${#item} ))
         if (( avail < 0 )); then
-            exit 0
+            break 2
         fi
 
         echo -n "${item}"
     done
 done
+
+# Newline if interactive
+if [ -t 1 ]; then
+    echo
+fi
