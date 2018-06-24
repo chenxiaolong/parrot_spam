1f38014d58819805692866e4071f6947bb53b9f0
Shlomo Shuck <sjshuck@us.ibm.com>
2018-06-24 18:39:28 -0400

    ruby: Tweak

--- parrot_spam.rb
+++ parrot_spam.rb
@@ -1,14 +1,14 @@
 #! /usr/bin/env ruby
 
 parrots = ARGV.reject(&:empty?)
-if parrots.empty?
+if parrots.none?
   STDERR.puts "Nothing to repeat"
   exit 1
 end
 
 chars_left = 4000
 
-parrots.cycle.each do |p|
+parrots.cycle do |p|
   chars_left -= p.length
   break if chars_left < 0
 
