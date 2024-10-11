0df955d935dc0771c14bd2c0a9cce9518c8baee9
Steven Shuck <stevenjshuck@gmail.com>
2024-10-11 14:56:49 -0400

    ParrotSpam.hs: Use type system moar

--- ParrotSpam.hs
+++ ParrotSpam.hs
@@ -1,17 +1,25 @@
-import Data.List          (scanl')
-import System.Environment (getArgs)
-import System.Exit        (die)
+import           Data.Function      ((&))
+import           Data.List          (scanl')
+import           Data.List.NonEmpty (NonEmpty, nonEmpty)
+import qualified Data.List.NonEmpty as NE
+import           Data.Maybe         (mapMaybe)
+import           System.Environment (getArgs)
+import           System.Exit        (die)
 
 main :: IO ()
 main = getArgs >>= validate >>= putStrLn . spam 4000
 
-validate :: [String] -> IO [String]
-validate args = case filter (not . null) args of
-    []      -> die "Nothing to repeat"
-    parrots -> return parrots
+-- | Guarantee using the type system that each parrot is non-empty and there's
+-- at least one
+validate :: [String] -> IO (NonEmpty (NonEmpty Char))
+validate args = case nonEmpty $ mapMaybe nonEmpty args of
+    Nothing      -> die "Nothing to repeat"
+    Just parrots -> return parrots
 
-spam :: Int -> [String] -> String
-spam maxChars parrots =
-    let charsUsed = tail $ scanl' (+) 0 $ cycle $ map length parrots
-        parrotsCharsUsed = zip (cycle parrots) charsUsed
-    in concatMap fst $ takeWhile (\(_, n) -> n <= maxChars) parrotsCharsUsed
+spam :: Int -> NonEmpty (NonEmpty Char) -> String
+spam maxChars parrots = parrots
+    & NE.map ((,) <*> NE.length)                     -- annotate with lengths
+    & NE.cycle                                       -- cycle infinitely
+    & NE.scanl1 (\(_, used) (p, l) -> (p, used + l)) -- cumulative lengths
+    & NE.takeWhile (\(_, used) -> used <= maxChars)  -- until char limit
+    & concatMap (NE.toList . fst)                    -- collect as string
